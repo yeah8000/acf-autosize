ce149e6ac8e50b4d0c4ae1d999fbb5eca5339c40
Claus Hoffmann <claus@yeah.de>
2019-11-02 15:04:39 +0100

    Fix: update textareas on load and on creation in repeater or flexcontent fields

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
 		'no-tabs': 0
 	},
 	globals: {
-		ACFAutosize: false
+		ACFAutosize: false,
+		acf: false
 	}
 }
--- src/js/textarea.js
+++ src/js/textarea.js
@@ -3,4 +3,18 @@ const autosize = require('autosize');
 ($ => {
 	const textareas = $('.acf-field.autosize textarea')
 	autosize(textareas)
+
+	// safety first: update on ready and load.
+	$(document).ready(() => {
+		autosize.update(textareas)
+	})
+	$(window).on('load', () => {
+		autosize.update(textareas)
+	})
+
+	// init autosize on newly created repeater/flexcontent fields
+	acf.add_action('append', function ($el) {
+		let textarea = $el.find('.acf-field.autosize textarea')
+		autosize(textarea)
+	})
 })(window.jQuery)
