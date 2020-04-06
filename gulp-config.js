2f02555dfd7fe868bc9a73d10fa5d41276b20141
Claus Hoffmann <claus@yeah.de>
2020-04-06 10:57:12 +0200

    update gulp workflow to fix security issues

--- gulp-config.js
+++ gulp-config.js
@@ -1,82 +1,9 @@
-const imagemin = require('gulp-imagemin')
-const browserlist = ['> 0.1%']
 
 module.exports = {
-	css: {
-		scss: {
-			config: {
-				outputStyle: 'compressed' // nested, compact, expanded and compressed are available options
-			}
-		},
-
-		sourcemaps: {
-			enabled: 'dev'
-		},
-
-		autoprefixer: {
-			enabled: true,
-			config: {
-				browserlist: browserlist
-			}
-		},
-
-		cleanCss: {
-			enabled: true,
-			config: {
-				compatibility: 'ie8'
-			}
-		}
-	},
-
-	js: {
-		sourcemaps: {
-			enabled: 'dev'
-		},
-		browserify: {
-			enabled: true
-		},
-
-		babeljs: {
-			enabled: true,
-			config: {
-				minified: true,
-				comments: false,
-				presets: [
-					[
-						'@babel/preset-env',
-						{
-							targets: {
-								browsers: browserlist
-							}
-						}
-					]
-				]
-			}
-		}
-	},
 
 	clean: {
 		enabled: 'dist',
 		paths: ['./public/**/*.map', './src/tmp']
-	},
-
-	images: {
-		imagemin: {
-			enabled: true,
-			config: [
-				imagemin.gifsicle({ interlaced: true }),
-				imagemin.jpegtran({ progressive: true }),
-				imagemin.optipng({ optimizationLevel: 5 }),
-				imagemin.svgo({})
-			]
-		}
-	},
-
-	svg: {
-		svgmin: {
-			enabled: true,
-			config: {}
-		}
 	},
 
 	paths: {
